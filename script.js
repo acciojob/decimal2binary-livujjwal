ad99523f01d81a3781c965865528c7864609b38a
Ujjwal Singh <132872642+livujjwal@users.noreply.github.com>
2023-08-12 16:44:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function decimalToBinary(num) {
-	if (num == 0 || num == 1) {
-		return num;
-	}
-	let binary = '';
- while (num>0) {
- 	binary += num%2;
-	 num /=2;
- }
-  return binary;
-}
+ function decimalToBinary(num) {
+    if (num == 0 || num == 1) {
+      return num;
+    }
+    let binary = [];
+   while (num>0) {
+      binary.unshift(Math.floor(num%2));
+     num = Math.floor(num/2);
+     console.log(num)
+   }
+    console.log(binary.join(''));
+    return binary;
+  }
 
 window.decimalToBinary = decimalToBinary;
